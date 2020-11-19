d97341552af5ec2fa1b5b7efb333cd3241216947
Nic Barnes <colorpulse@gmail.com>
2020-11-19 10:33:04 +0100

    navbar complete

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -1,10 +1,27 @@
-import headshot from './images/headshot.jpg';
-import heroBackground from './images/hero-background.jpg';
-import './App.scss';
+import headshot from "./images/headshot.jpg";
+import heroBackground from "./images/hero-background.jpg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
+      <nav className="nav-bar">
+        <div className="nav-header">
+          <h1>Travelize</h1>
+          <h3>
+            <em>My traveling experiences</em>
+          </h3>
+        </div>
+        <div>
+          <ul className="nav-links">
+            <li>Home</li>
+            <li>Blog</li>
+            <li className="active-nav">About</li>
+          </ul>
+        </div>
+      </nav>
+      
+      
     </div>
   );
 }
